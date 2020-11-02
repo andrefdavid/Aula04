15a8260bc491a8c4fad762974aea1ab781bf5c2c
André David <andre.david@fiap.com.br>
2020-11-02 15:24:04 -0300

    Projeto com diversos commits para navegar

--- src/NavegandoCommits.java
+++ src/NavegandoCommits.java
@@ -2,9 +2,7 @@
 public class NavegandoCommits {
 
 	public static void main(String[] args) {
-		/*Navegar entre commits é importantíssimo!
-		 * Não vamos escrever código nesse arquivo... 
-		 * Ele é apenas para te mostrar a importância de poder navegar.*/
+		/*Cadê o conteúdo que estava aqui?*/
 
 	}
 
