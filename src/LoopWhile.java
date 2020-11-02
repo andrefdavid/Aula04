fcf3ff6cb87547b982c8291d236a1b707c4d1897
André David <andre.david@fiap.com.br>
2020-11-02 15:16:17 -0300

    Descricao do problema no arquivo LoopWhile.java

--- src/LoopWhile.java
+++ src/LoopWhile.java
@@ -2,7 +2,8 @@
 public class LoopWhile {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*Imagine que uma ONG precise saber a média de idades dos alunos de uma escola.
+		 * Sabendo de antemão quantos alunos existem e sem a necessidade de armazenar permanentemente a idade de cada um desses alunos, qual é a melhor forma de calcular essa média?*/
 
 	}
 
